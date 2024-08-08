f93bc7d1e46c9633548f15ade2f0c7a2eba6a205
Surya <suryateja.kandukuru@techolution.com>
2024-08-08 23:29:12 +0530

    feat: backend bare minimum setup

---
+++ backend/src/index.ts
@@ -0,0 +1,15 @@
+import dotenv from "dotenv";
+import express, { type Request, type Response } from "express";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (_req: Request, res: Response) => {
+	res.send("Hello World!");
+});
+
+app.listen(port, () => {
+	console.log(`[server]: Server is running at http://localhost:${port}`);
+});
