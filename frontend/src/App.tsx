09676028977e19cae83420ca3b3b686f68cd116d
Surya <suryateja.kandukuru@techolution.com>
2024-08-09 00:10:17 +0530

    feat: bare min setup for frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+	return (
+		<div className="flex justify-center items-center bg-red-600 text-4xl">
+			App
+		</div>
+	);
+};
+
+export default App;
